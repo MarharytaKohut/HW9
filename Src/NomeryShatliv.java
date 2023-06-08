39750ad58a38257273104b56324638a3350d37b6
MarharytaKohut <mrhrtkohut@gmail.com>
2023-06-09 02:58:20 +0300

    commit

---
+++ Src/NomeryShatliv.java
@@ -0,0 +1,10 @@
+public class NomeryShatliv {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if ( i != 4 & i != 9) {
+                System.out.println(i);
+            }
+                }
+
+            }
+        }
